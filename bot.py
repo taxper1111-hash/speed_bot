ddb27ec76b87daad4018b0076ad4aa80661940ae
taxper1111-hash <taxper1111@gmail.com>
2026-02-03 02:29:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,102 +1,99 @@
 import discord
-from discord.ext import commands
-import random
 import asyncio
 import os
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-intents.presences = True
+crazy_mode = True
+last_target_id = None
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-running = False
-
-CORE_LINES = [
+normal_lines = [
     "MY MOM IS KINDA HOMELESS",
     "I LIVE WITH MY DAD",
-    "I JUST WANNA HELP MY MOM",
+    "IM TRYING TO HELP MY MOM",
     "THIS IS REAL LIFE",
-    "IM NOT ROLEPLAYING",
     "BRO IM SERIOUS"
 ]
 
-REJECTED_LINES = [
-    "OKAY MAYBE I SHOULDNT HAVE SAID ANYTHING",
-    "YALL DONT CARE ANYWAY",
-    "IM ACTUALLY STUPID FOR SAYING THIS",
-    "WHY DO I EVEN TALK",
-    "NEVER MIND BRO",
-    "IM DONE TALKING"
-]
-
-CHAOS_LINES = [
-    "WHY YALL LAUGHING",
+crazy_lines = [
+    "PLS I NEED THIS",
     "STOP TYPING LMAO",
-    "BROOOOO",
-    "THIS CHAT IS EVIL",
-    "NAH THIS CRAZY"
+    "IM NOT ROLEPLAYING",
+    "NAH THIS CRAZY",
+    "BRO PLEASE",
+    "THIS IS REAL LIFE",
+    "IM SHAKING RN",
+    "WHY NOBODY HELPING",
+    "IM DEAD SERIOUS",
+    "PLEASE BRO PLEASE"
 ]
 
-EMOJIS = ["😭", "💀", "🤡", "😔", "😡"]
-
-def build_message(name):
-    lines = []
-    lines.append(f"PLS @{name} I NEED THIS")
-    for _ in range(random.randint(2, 4)):
-        lines.append(random.choice(CORE_LINES))
-    if random.random() < 0.4:
-        lines.append(random.choice(REJECTED_LINES))
-    else:
-        lines.append(random.choice(CHAOS_LINES))
-    lines.append(random.choice(EMOJIS) * random.randint(2, 5))
-    return "\n".join(lines)
-
-async def speed_loop():
-    global running
-    await bot.wait_until_ready()
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel is None:
-        return
-    while running:
-        members = [
-            m for m in channel.guild.members
-            if not m.bot and m.status != discord.Status.offline
-        ]
-        if members:
-            target = random.choice(members)
-            await channel.send(build_message(target.display_name))
-        await asyncio.sleep(random.randint(1, 4) * 60)
-
-@bot.command()
-async def start(ctx):
-    global running
-    if running:
-        await ctx.send("ALREADY RUNNING BRO")
-        return
-    running = True
-    bot.loop.create_task(speed_loop())
-    await ctx.send("SPEED MODE ON")
-
-@bot.command()
-async def stop(ctx):
-    global running
-    running = False
-    await ctx.send("SPEED MODE OFF")
-
-@bot.command()
-async def alive(ctx):
-    await ctx.send("THIS IS SPEED BOT IM ALIVE")
-
-@bot.event
+@client.event
 async def on_ready():
-    print(f"LOGIN OK {bot.user}")
+    print("BOT READY")
+    client.loop.create_task(loop_task())
 
-bot.run(TOKEN)
+async def loop_task():
+    await client.wait_until_ready()
+    global last_target_id
+    while not client.is_closed():
+        await asyncio.sleep(random.randint(60, 240))
+        if not crazy_mode:
+            continue
+        channel = client.get_channel(CHANNEL_ID)
+        if channel is None:
+            continue
+        members = [m for m in channel.guild.members if not m.bot]
+        if not members:
+            continue
+        target = random.choice(members)
+        last_target_id = target.id
+        mention = target.mention
+        msg = f"PLS {mention} I NEED THIS\n"
+        msg += "I LIVE WITH MY DAD\n"
+        msg += "MY MOM IS KINDA HOMELESS\n"
+        msg += random.choice(normal_lines)
+        await channel.send(msg)
+
+@client.event
+async def on_message(message):
+    global crazy_mode
+    if message.author.bot:
+        return
+
+    if message.content.lower() == "!alive":
+        await message.channel.send("THIS IS SPEED BOT IM ALIVE")
+        return
+
+    if message.content.lower() == "!crazy on":
+        crazy_mode = True
+        await message.channel.send("CRAZY MODE ON 😭🔥")
+        return
+
+    if message.content.lower() == "!crazy off":
+        crazy_mode = False
+        await message.channel.send("CRAZY MODE OFF 😐")
+        return
+
+    if last_target_id and message.author.id == last_target_id:
+        if message.content.lower() in ["no", "stop", "shut up", "wtf"]:
+            mention = message.author.mention
+            spam = f"PLS {mention} I NEED THIS\n"
+            spam += "THIS IS REAL LIFE\n"
+            spam += "MY MOM IS KINDA HOMELESS\n"
+            spam += "BRO IM SERIOUS\n"
+            spam += random.choice(crazy_lines)
+            await message.channel.send(spam)
+
+client.run(TOKEN)
 
 
 
